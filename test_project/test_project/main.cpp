b03d9cb4886a9cce699aab9f3565f7df36ec49ec
CYC <christopher.w.lu@gmail.com>
2016-02-12 20:34:28 -0500

    s

---
+++ test_project/test_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_project
+//
+//  Created by CYC on 2/12/16.
+//  Copyright © 2016 CYC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
